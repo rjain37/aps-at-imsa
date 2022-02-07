69432e6a1c183cd926ea00870b9d47290df8ee10
Rohan Jain <rjain@imsa.edu>
2022-02-06 23:26:41 -0600

    Small and incomplete outline

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const CsvReadableStream = require('csv-reader');
+const http = require('http').createServer(app);
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+//let subjectNames = fs.readFileSync(path.join(__dirname, '/subjects.txt'), 'utf8').split("\n");
+
+app.get('/', (req, res) => {
+    res.set('Cache-Control', 'public, max-age=25200');
+    res.render('pages/main.ejs');
+});
+
+app.get('/about', (req, res) => {
+    res.render('pages/about.ejs');
+});
+
+// ending
+const server = http.listen(8080, () => {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log(`App listening at http://localhost:${port}`);
+});
