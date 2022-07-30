08f8cec15d8807b990b57b3c1cf20c8b4c6208cb
Rohan Jain <rjain@imsa.edu>
2022-07-29 22:05:50 -0500

    Compare page added, not fully functional

--- index.js
+++ index.js
@@ -50,8 +50,54 @@ app.get('/data', (req, res) => {
 app.get("/compare", (req, res) => {
     res.set('Cache-Control', 'public, max-age=25200');
 
-    res.status(404).render("pages/compare.ejs", {
+    res.status(200).render("pages/compare.ejs", {
         exams: formattedNames
+    });
+})
+
+app.get("/compare/*", async (req, res) => {
+    res.set('Cache-Control', 'public, max-age=25200');
+
+    let exams = req.url.substring(1).split('++');
+    if(exams[exams.length - 1] == '') exams.pop();
+    let n = exams.length;
+    exams[0] = exams[0].substring(8); // get rid of the /compare
+
+    // examdata item format: [name, avg, pass rate, number of 5s, number of test-takers]
+    const nData = 5; // number of relevant stat categories, as shown above
+    examdata = [new Array(nData).fill('n/a'), new Array(nData).fill('n/a'), new Array(nData).fill('n/a')]
+
+    // put exam names into examdata
+    let i = 0
+    exams.forEach((exam) => {
+        examdata[i][0] = exam.split('+').join(' ');
+        i++
+    });
+
+    scoreArrays = []
+    i = 0;
+    for(let exam of exams){
+        scoreArrays.push(await getExamData(decodeURI(exam.split('+').join('%20'))));
+        examdata[i][1] = getAverageScore(scoreArrays[i]);
+        examdata[i][2] = String(getPassRate(scoreArrays[i])) + '%';
+        examdata[i][3] = scoreArrays[i][4];
+        examdata[i][4] = sampleSize(scoreArrays[i]);
+        if(examdata[i][4] == 0){
+            examdata[i][1] = 'n/a';
+            examdata[i][2] = 'n/a';
+            examdata[i][3] = 'n/a';
+        }
+        i++;
+    }
+
+    console.log(examdata);
+    
+
+    res.status(200).render("pages/compare2.ejs", {
+        exams: formattedNames,
+        exam1data: examdata[0],
+        exam2data: examdata[1],
+        exam3data: examdata[2]
     });
 })
 
@@ -141,6 +187,11 @@ function sampleSize(arr){
 //weighted average of array based on indices
 function getAverageScore(arr){
     return Math.round(100 * (1 * arr[0] + 2 * arr[1] + 3 * arr[2] + 4 * arr[3] + 5 * arr[4]) / sampleSize(arr))/100;
+}
+
+//percentage of 3s, 4s, and 5s
+function getPassRate(arr){
+    return Math.trunc(((arr[2] + arr[3] + arr[4]) / sampleSize(arr)) * 10000) / 100;
 }
 
 async function getMostPopularExam() {
